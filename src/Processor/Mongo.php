8eec4c66115ee2c4cc06c2fff26702594631e5b7
Nick Ilyin <nick.ilyin@gmail.com>
2013-03-05 15:42:55 -0500

    Added an id for server for MongoDB and fixed a PHP 5.2.17 incompatible call: $name::method().

--- src/Processor/Mongo.php
+++ src/Processor/Mongo.php
@@ -5,12 +5,16 @@ class N7_Processor_Mongo implements N7_ProcessorInterface
 	protected $mongo;
 	protected $db;
 	protected $col;
+	protected $serverId;
 	
-	public function __construct($constring = 'mongodb://localhost:27017', $db = 'log', $collection = 'log')
+	public function __construct($constring = 'mongodb://localhost:27017', $db = 'log', $collection = 'log', $serverId = null)
 	{
 		$this->mongo = new MongoClient($constring);
 		$this->db = $this->mongo->selectDB($db);
 		$this->col = $this->db->$collection;
+		
+		if ($serverId)
+		  $this->serverId = $serverId;
 	}
 	public function write(N7_LogEntryInterface $entry)
 	{
@@ -21,6 +25,8 @@ class N7_Processor_Mongo implements N7_ProcessorInterface
 			if ($val === null)
 				unset($map[$key]);
 		}
+		
+		$map['server_id'] = $this->serverId;
 		
 		//make a few tweaks
 		$map['timestamp'] = new MongoDate($map['timestamp']);
